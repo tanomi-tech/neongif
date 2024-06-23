4cc7b28ba5fc2728b61677d2e49d56cf66444152
jesse <me@jessebarkdoll.com>
2024-06-23 17:48:21 -0400

    progress on v1

---
+++ lib.js
@@ -0,0 +1,88 @@
+/**
+ * @link https://stackoverflow.com/a/26215431
+ */
+const kebabToCamel = (o) => {
+  let newO, origKey, newKey, value;
+  if (o instanceof Array) {
+    return o.map((value) => {
+        if (typeof value === "object") {
+          value = kebabToCamel(value);
+        }
+        return value;
+    });
+  } else {
+    newO = {};
+    for (origKey in o) {
+      if (o.hasOwnProperty(origKey)) {
+        newKey = origKey.split('-').map((s, i) => i === 0 ? s : s[0].toUpperCase() + s.slice(1).toLowerCase()).join('');
+        value = o[origKey];
+        if (value instanceof Array || (value !== null && value.constructor === Object)) {
+          value = kebabToCamel(value);
+        }
+        newO[newKey] = value;
+      }
+    }
+  }
+  return newO;
+}
+
+const isFrameMetadata = (logMessage) => logMessage.startsWith('frame=');
+
+const initProgressSection = () => {
+  // Create the outer div with class "progress-section"
+  const progressSection = document.createElement('div');
+  progressSection.classList.add('progress-section');
+
+  // Create the span with id "progress-text"
+  const progressText = document.createElement('span');
+  progressText.classList.add('progress-text');
+  progressText.innerText = '0%';
+  progressSection.appendChild(progressText);
+  
+  // Create the inner div with id "progress-bar"
+  const progressBar = document.createElement('div');
+  progressBar.classList.add('progress-bar');
+
+  // Create the span with id "progress-value" inside the "progress-bar" div
+  const progressValue = document.createElement('span');
+  progressValue.classList.add('progress-value');
+  progressBar.appendChild(progressValue);
+
+  // Append the "progress-bar" div to the "progress-section" div
+  progressSection.appendChild(progressBar);
+
+  return progressSection;
+}
+
+const calcTotalDuration = (hours, minutes, seconds) => 
+  (hours * 60 * 60) + (minutes * 60) + seconds;
+
+const parseTimestamp = (hours, minutes, seconds) => 
+  [hours, minutes, seconds]
+    .map(shard => shard.padStart(2, '0'))
+    .join(':');
+
+const createUpdateProgressCb = (progressSectionNode, clipDuration) => frame => {
+  const progValue = progressSectionNode.querySelector('.progress-value');
+  const progTxt = progressSectionNode.querySelector('.progress-text');
+  /**
+   * we need the x10 multiplier here because 
+   * our ffmpeg.exec(...) command uses a param 10 fps
+   */
+  const progress = Math.round((parseInt(frame) / (clipDuration * 10)) * 100);
+
+  if (progValue.dataset.value !== progress) {
+    progValue.style.width = progress + '%';
+    progTxt.innerText = progress + '%';
+    console.log(`${progress}% complete...`)
+  }
+}
+
+export {
+  calcTotalDuration,
+  createUpdateProgressCb,
+  initProgressSection,
+  isFrameMetadata,
+  kebabToCamel,
+  parseTimestamp,
+}
--- main.js
+++ main.js
@@ -2,12 +2,42 @@ import { showSaveFilePicker } from './node_modules/native-file-system-adapter/mo
 import { FFmpeg } from "./node_modules/@ffmpeg/ffmpeg/dist/esm/index.js";
 import { fetchFile, toBlobURL } from "./node_modules/@ffmpeg/util/dist/esm/index.js";
 import { fileSave } from './node_modules/browser-fs-access/dist/index.modern.js';
+import { 
+  calcTotalDuration, 
+  createUpdateProgressCb,
+  initProgressSection,
+  isFrameMetadata,
+  kebabToCamel,
+  parseTimestamp,
+} from './lib.js';
+import { fromEvent, combineLatest, startWith, merge, map, tap } from './node_modules/rxjs/dist/esm';
 
+const convertButton = document.getElementById('convert-button');
 const convertForm = document.getElementById('convert-form');
-const fileInput = document.getElementById('file-input'); 
 const previewWindow = document.getElementById('preview-window');
-const progBar = document.getElementById('progress-bar');
-const progTxt = document.getElementById('progress-text');
+
+// Form Inputs
+const fileInput = document.getElementById('file-input'); 
+const hoursInput = document.querySelector('input[name="start-time-hours"]');
+const minutesInput = document.querySelector('input[name="start-time-minutes"]');
+const secondsInput = document.querySelector('input[name="start-time-seconds"]');
+const durationInput = document.querySelector('input[name="duration"]');
+
+let file, totalDurationSeconds, capturedDuration = null;
+
+const timeInputEventStreams = [hoursInput, minutesInput, secondsInput]
+  .map(input => fromEvent(input, 'input')
+  .pipe(startWith(null)));
+
+combineLatest(timeInputEventStreams)
+  .pipe(
+    map(events => events.map(e => e?.target?.value || null)),
+  )
+  .subscribe((timeSlots) => {
+    const [hours, minutes, seconds] = timeSlots.map(slot => parseFloat(slot ?? '0'));
+    const startTimeTotal = calcTotalDuration(hours, minutes, seconds);
+    durationInput.max = Math.max(0, (totalDurationSeconds ?? 0) - startTimeTotal);
+  });
 
 const BASE_URL = 'https://unpkg.com/@ffmpeg/core@0.12.6/dist/esm';
 const ffmpeg = new FFmpeg({ log: true });
@@ -17,41 +47,71 @@ await ffmpeg.load({
   wasmURL: await toBlobURL(`${BASE_URL}/ffmpeg-core.wasm`, 'application/wasm'),
 });
 
-ffmpeg.on("log", ({ message }) => {
-  console.log(message);
-});
+ffmpeg.on("log", (log) => {
+  if (!capturedDuration && log.message.trim().startsWith('Duration:')) {
+    const durationSegments = log.message
+      ?.trim()
+      ?.split(/[,:]/)
+      ?.slice(1, 4)
+      ?.map(shard => shard.trim())
+      ?.map(shard => parseFloat(shard));
 
-ffmpeg.on("progress", ({ progress }) => {
-  const progRounded = Math.round(progress * 100);
-  if (progBar.value !== progRounded) {
-    progBar.value = progRounded;
-    progBar.innerText = progRounded + '%';
-    progTxt.innerText = progRounded + '%';
-    console.groupEnd();
-    console.group();
-    console.log(`${progRounded}% complete...`)
+    const [hours, minutes, seconds] = durationSegments;
+    totalDurationSeconds = calcTotalDuration(hours, minutes, seconds);
+
+    hoursInput.max = hours;
+    minutesInput.max = Math.min(59.99, (hours * 60) + minutes);
+    secondsInput.max = Math.min(59.99, totalDurationSeconds);
+
+    capturedDuration = durationSegments?.join(':') ?? null;
   }
+  
+  console.log(log.message);
 });
 
-async function processLargeVideo(file) {
-  const buff = await file.arrayBuffer();
-  await ffmpeg.writeFile(file.name, new Uint8Array(buff));
+// ffmpeg.on("progress", ({ progress }) => {});
+
+async function processLargeVideo(submission) {
+  const { 
+    startTimeHours: hh, 
+    startTimeMinutes: mm, 
+    startTimeSeconds: ss, 
+    duration, 
+    videoFile: file 
+  } = submission;
+
+  const timestamp = parseTimestamp(hh, mm, ss);
+  console.log(timestamp, duration, file);
   const basename = file.name.split('.');
   basename.pop();
   const outputName = `${basename.join()}.gif`;
   const outputWidth = '640';
 
+  const progressSectionNode = initProgressSection();
+  previewWindow.replaceChildren(progressSectionNode);
+
+  const updateProgress = createUpdateProgressCb(progressSectionNode, duration);
+
+  const handleFrameProgress = log => {
+    if (!isFrameMetadata(log.message)) {
+      return;
+    }
+    const frame = log.message.split(/\s+/)?.[1];
+    if (!isNaN(frame)) updateProgress(frame);
+  };
+
+  ffmpeg.on('log', handleFrameProgress);
   console.group();
   await ffmpeg.exec([
-    '-threads', 
-    Math.min(navigator.hardwareConcurrency, 4).toString(),
-    '-i',
-    file.name,
-    '-vf', 
-    `fps=10,scale=${outputWidth}:-1:flags=lanczos`,
+    '-threads', Math.min(navigator.hardwareConcurrency, 4).toString(),
+    '-ss', timestamp,
+    '-i', file.name,
+    '-to', duration,
+    '-vf', `fps=10,scale=${outputWidth}:-1:flags=lanczos`,
     outputName
   ]);
   console.groupEnd();
+  ffmpeg.off('log', handleFrameProgress)
 
   const outputData = await ffmpeg.readFile(outputName);
   const blob = new Blob([outputData.buffer], {type: 'image/gif'});
@@ -67,28 +127,36 @@ async function processLargeVideo(file) {
   downloadButton.href = outputUrl;
   downloadButton.download = `${basename}.gif`;
 
-  previewWindow.parentElement.append(downloadButton);
+  previewWindow.append(downloadButton);
 
   await ffmpeg.deleteFile(file.name);
   await ffmpeg.deleteFile(outputName);
   console.log('File has been written successfully.');
 }
 
-let file;
-fileInput.addEventListener('change', (event) => {
+fileInput.addEventListener('change', async (event) => {
   file = event.target.files[0];
+  convertButton.disabled = !file;
+
+  const buff = await file.arrayBuffer();
+  await ffmpeg.writeFile(file.name, new Uint8Array(buff));
+
+  capturedDuration = null;
+  await ffmpeg.exec([
+    '-i',
+    file.name,
+  ]);
 });
 
 convertForm.addEventListener('submit', (event) => {
   event.preventDefault();
-  if (!file) return false; 
+  const submission = kebabToCamel(Object.fromEntries(new FormData(event.target).entries()));
+  if (!submission.videoFile) return false; 
 
-  const convertButton = document.getElementById('convert-button');
   convertButton.disabled = true;
   convertButton.innerText = 'Converting...'
-  progTxt.classList.add('active');
-  progTxt.innerText = '0%';
-  processLargeVideo(file)
+
+  processLargeVideo(submission)
     .then(() => {
       convertButton.disabled = false;
       convertButton.innerText = 'Convert';
