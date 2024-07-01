96f598069b3d7986673b7f292cf750422083d008
jesse <me@jessebarkdoll.com>
2024-06-30 23:18:15 -0400

    pugify site

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import mkcert from 'vite-plugin-mkcert'
+import pugPlugin from 'vite-plugin-pug'
 
 export default defineConfig({
   server: { https: true },
-  plugins: [ mkcert() ]
+  plugins: [ mkcert(), pugPlugin() ],
 });
