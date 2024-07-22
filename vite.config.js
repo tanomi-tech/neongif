b4bbd3c7afa2709f79a289e861e19b2424830450
jesse <me@jessebarkdoll.com>
2024-07-21 22:08:31 -0400

    bundle node modules

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,32 @@
 import { defineConfig } from 'vite'
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import mkcert from 'vite-plugin-mkcert'
 import pugPlugin from 'vite-plugin-pug'
 
 export default defineConfig({
-  server: { https: true },
-  publicDir: 'public',
-  plugins: [ mkcert(), pugPlugin() ],
+  server: {
+    https: true,
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+    },
+  },
+  optimizeDeps: {
+    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util'],
+  },
+  plugins: [ mkcert(), pugPlugin(), nodeResolve() ],
   build: {
     target: 'es2022',
     rollupOptions: {
       input: ['index.html', 'public/main.js'],
       output: {
-        entryFileNames: '[name].js'
-      }
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+        entryFileNames: '[name].js',
+      },
     },
   },
+  worker: {
+    format: 'es',
+  }
 });
